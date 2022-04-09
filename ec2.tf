918a45e70ff99d731298e17b1140992e562081c0
aaronholt1066 <aholt@oakridge-it.com>
2022-04-09 08:41:08 +0100

    initial commit

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "my_test_ldn" {
+  ami                         = var.ldn-ami-id
+  subnet_id                   = var.london-subnet-id
+  instance_type               = var.instance-type
+  key_name                    = "OakridgeAWSLinuxKey"
+  associate_public_ip_address = true
+
+  tags = {
+      drift_example = "v1"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ldn-ami-id" {
+  default = "ami-060c4f2d72966500a"
+}
+
+variable "london-subnet-id" {
+  default = "subnet-d683309a"
+}
+
+variable "instance-type" {
+  default = "ami-080af029940804103"
+}
